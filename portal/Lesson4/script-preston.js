ca7ab9e3e06defdc581f296f1de8c3e91eb948e3
Nastyonish <kudasovaa@list.ru>
2020-02-06 21:48:30 +0300

    lesson4

---
+++ portal/Lesson4/script-preston.js
@@ -0,0 +1,6 @@
+let lastModif = document.lastModified;
+document.getElementById('lastModified').innerHTML = lastModif;
+
+function Menu() {
+    document.getElementById("menu").classList.toggle("hide")
+}
