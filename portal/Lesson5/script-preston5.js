3d76ac5c9cf3abed4c4fd628d8bedb0ea6131673
Nastyonish <kudasovaa@list.ru>
2020-02-09 21:44:44 +0300

    l5

--- portal/Lesson5/script-preston5.js
+++ portal/Lesson5/script-preston5.js
@@ -4,3 +4,9 @@ document.getElementById('lastModified').innerHTML = lastModif;
 function Menu() {
     document.getElementById("menu").classList.toggle("hide")
 }
+
+let d = new Date();
+let week = d.getDay();
+if(week !== 5) {
+    document.getElementById("promo").style.display = "none";
+}
