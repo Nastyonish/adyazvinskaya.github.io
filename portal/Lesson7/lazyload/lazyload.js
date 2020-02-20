07405a8b3cfd7b8be68da425d5548b37ff09c879
Nastyonish <kudasovaa@list.ru>
2020-02-20 14:46:34 +0300

    lazyload

--- portal/Lesson7/lazyload/lazyload.js
+++ portal/Lesson7/lazyload/lazyload.js
@@ -3,44 +3,11 @@ let lastModif = document.lastModified;
 document.getElementById('lastModified').innerHTML = lastModif;
 
 // Lazyload
-const images = document.querySelectorAll("[data-src]");
-
-function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if (!src) {
-        return;
-    }
-    img.src = src;
-}
-
-const imgOptions = {
-    threshold: 1,
-    rootMargin: "0px 0px 200px 0px"
-};
-
-const imgObserver = new IntersectionObserver((entries, 
-    imgObserver) => {
-        entries.forEach(entry => {
-            if (!entry.isIntersecting) {
-                return;
-            } else {
-                preloadImage(entry.target);
-                imgObserver.unobserve(entry.target);
-            }
-        })
-}, imgOptions);
-
-images.forEach((image) => {
-    imgObserver.observe(image);
-});
-
-
-/*
 const imagesToLoad = document.querySelectorAll("img[data-src]");
 
 const imgOptions = {
     threshold: 0.8,
-    rootMargin: "0px 0px 50px 0px"
+    rootMargin: "0px 0px 200px 0px"
 };
 
 
@@ -70,4 +37,3 @@ else {
         loadImages(img)
     })
 }
-*/
