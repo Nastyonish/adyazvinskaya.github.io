3493c11cb5ffccc0c81d804b102d2f3482a29ce5
Nastyonish <kudasovaa@list.ru>
2020-02-22 00:36:45 +0300

    gallery

--- portal/Lesson7/gallery/gallery.js
+++ portal/Lesson7/gallery/gallery.js
@@ -1,6 +1,8 @@
+// Last Modifications
 let lastModif = document.lastModified;
 document.getElementById('lastModified').innerHTML = lastModif;
 
+// Menu
 function Menu() {
     document.getElementById("menu").classList.toggle("hide")
 }
@@ -9,4 +11,38 @@ let d = new Date();
 let week = d.getDay();
 if(week !== 5) {
     document.getElementById("promo").style.display = "none";
+}
+
+// Lazyload
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 0px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src');};
+};
+
+
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item => {
+            if (item.isIntersecting) {
+                loadImages(item.target)
+                observer.unobserve(item.target)
+            }
+        }))
+    }, imgOptions);
+
+    imagesToLoad.forEach(img => {
+        imgObserver.observe(img);
+    });
+} 
+else {
+    imagesToLoad.forEach(img => {
+        loadImages(img)
+    })
 }
